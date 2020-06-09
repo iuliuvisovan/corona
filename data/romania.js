38c2bfabaca417f17c25779261d5ba6668623447
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-06-09 19:38:31 +0300

    new script boy

--- data/romania.js
+++ data/romania.js
@@ -1,522 +1,522 @@
 window.romaniaData = {
-  '09/06/2020': {
-    cases: 145,
-    deaths: 11,
-    recoveries: 84,
-  },
-  '08/06/2020': {
-    cases: 125,
-    deaths: 8,
-    recoveries: 188,
-  },
-  '07/06/2020': {
-    cases: 189,
-    deaths: 8,
-    recoveries: 219,
-  },
-  '06/06/2020': {
-    cases: 187,
-    deaths: 10,
-    recoveries: 274,
-  },
-  '05/06/2020': {
-    cases: 196,
-    deaths: 9,
-    recoveries: 226,
-  },
-  '04/06/2020': {
-    cases: 238,
-    deaths: 11,
-    recoveries: 119,
-  },
-  '03/06/2020': {
-    cases: 152,
-    deaths: 9,
-    recoveries: 274,
-  },
-  '02/06/2020': {
-    cases: 119,
-    deaths: 9,
-    recoveries: 100,
-  },
-  '01/06/2020': {
-    cases: 141,
-    deaths: 8,
-    recoveries: 170,
-  },
-  '31/05/2020': {
-    cases: 124,
-    deaths: 9,
-    recoveries: 210,
-  },
-  '30/05/2020': {
-    cases: 151,
-    deaths: 13,
-    recoveries: 217,
-  },
-  '29/05/2020': {
-    cases: 191,
-    deaths: 11,
-    recoveries: 200,
-  },
-  '28/05/2020': {
-    cases: 197,
-    deaths: 10,
-    recoveries: 467,
-  },
-  '27/05/2020': {
-    cases: 165,
-    deaths: 9,
-    recoveries: 288,
-  },
-  '26/05/2020': {
-    cases: 146,
-    deaths: 17,
-    recoveries: 244,
-  },
-  '25/05/2020': {
-    cases: 213,
-    deaths: 14,
-    recoveries: 231,
-  },
-  '24/05/2020': {
-    cases: 213,
-    deaths: 9,
-    recoveries: 212,
-  },
-  '23/05/2020': {
-    cases: 145,
-    deaths: 11,
-    recoveries: 410,
-  },
-  '22/05/2020': {
-    cases: 127,
-    deaths: 8,
-    recoveries: 196,
-  },
-  '21/05/2020': {
-    cases: 198,
-    deaths: 10,
-    recoveries: 225,
-  },
-  '20/05/2020': {
-    cases: 196,
-    deaths: 15,
-    recoveries: 190,
-  },
-  '19/05/2020': {
-    cases: 155,
-    deaths: 19,
-    recoveries: 236,
-  },
-  '18/05/2020': {
-    cases: 165,
-    deaths: 10,
-    recoveries: 40,
-  },
-  '17/05/2020': {
-    cases: 167,
-    deaths: 16,
-    recoveries: 316,
-  },
-  '16/05/2020': {
-    cases: 267,
-    deaths: 25,
-    recoveries: 204,
-  },
-  '15/05/2020': {
-    cases: 190,
-    deaths: 10,
-    recoveries: 317,
-  },
-  '14/05/2020': {
-    cases: 245,
-    deaths: 30,
-    recoveries: 1092,
-  },
-  '13/05/2020': {
-    cases: 214,
-    deaths: 25,
-    recoveries: 276,
-  },
-  '12/05/2020': {
-    cases: 200,
-    deaths: 19,
-    recoveries: 440,
-  },
-  '11/05/2020': {
-    cases: 226,
-    deaths: 20,
-    recoveries: 194,
-  },
-  '10/05/2020': {
-    cases: 231,
-    deaths: 26,
-    recoveries: 139,
-  },
-  '09/05/2020': {
-    cases: 320,
-    deaths: 28,
-    recoveries: 489,
-  },
-  '08/05/2020': {
-    cases: 312,
-    deaths: 22,
-    recoveries: 279,
-  },
-  '07/05/2020': {
-    cases: 392,
-    deaths: 18,
-    recoveries: 356,
-  },
-  '06/05/2020': {
-    cases: 270,
-    deaths: 31,
-    recoveries: 334,
-  },
-  '05/05/2020': {
-    cases: 325,
-    deaths: 24,
-    recoveries: 185,
-  },
-  '04/05/2020': {
-    cases: 349,
-    deaths: 23,
-    recoveries: 400,
-  },
-  '03/05/2020': {
-    cases: 431,
-    deaths: 25,
-    recoveries: 322,
-  },
-  '02/05/2020': {
-    cases: 165,
-    deaths: 29,
-    recoveries: 219,
-  },
-  '01/05/2020': {
-    cases: 327,
-    deaths: 31,
-    recoveries: 311,
-  },
-  '30/04/2020': {
-    cases: 262,
-    deaths: 20,
-    recoveries: 448,
-  },
-  '29/04/2020': {
-    cases: 362,
-    deaths: 25,
-    recoveries: 165,
-  },
-  '28/04/2020': {
-    cases: 277,
-    deaths: 19,
-    recoveries: 263,
-  },
-  '27/04/2020': {
-    cases: 303,
-    deaths: 23,
-    recoveries: 87,
-  },
-  '26/04/2020': {
-    cases: 401,
-    deaths: 33,
-    recoveries: 164,
-  },
-  '25/04/2020': {
-    cases: 218,
-    deaths: 23,
-    recoveries: 73,
-  },
-  '24/04/2020': {
-    cases: 321,
-    deaths: 25,
-    recoveries: 339,
-  },
-  '23/04/2020': {
-    cases: 386,
-    deaths: 19,
-    recoveries: 72,
-  },
-  '22/04/2020': {
-    cases: 468,
-    deaths: 26,
-    recoveries: 253,
-  },
-  '21/04/2020': {
-    cases: 306,
-    deaths: 31,
-    recoveries: 136,
-  },
-  '20/04/2020': {
-    cases: 190,
-    deaths: 17,
-    recoveries: 125,
-  },
-  '19/04/2020': {
-    cases: 328,
-    deaths: 17,
-    recoveries: 162,
-  },
-  '18/04/2020': {
-    cases: 351,
-    deaths: 17,
-    recoveries: 222,
-  },
-  '17/04/2020': {
-    cases: 360,
-    deaths: 13,
-    recoveries: 151,
-  },
-  '16/04/2020': {
-    cases: 491,
-    deaths: 25,
-    recoveries: 140,
-  },
-  '15/04/2020': {
-    cases: 337,
-    deaths: 30,
-    recoveries: 166,
-  },
-  '14/04/2020': {
-    cases: 246,
-    deaths: 14,
-    recoveries: 137,
-  },
-  '13/04/2020': {
-    cases: 333,
-    deaths: 12,
-    recoveries: 62,
-  },
-  '12/04/2020': {
-    cases: 310,
-    deaths: 24,
-    recoveries: 94,
-  },
-  '11/04/2020': {
-    cases: 523,
-    deaths: 25,
-    recoveries: 29,
-  },
-  '10/04/2020': {
-    cases: 265,
-    deaths: 28,
-    recoveries: 82,
-  },
-  '09/04/2020': {
-    cases: 441,
-    deaths: 20,
-    recoveries: 119,
-  },
-  '08/04/2020': {
-    cases: 344,
-    deaths: 27,
-    recoveries: 68,
-  },
-  '07/04/2020': {
-    cases: 360,
-    deaths: 25,
-    recoveries: 54,
-  },
-  '06/04/2020': {
-    cases: 193,
-    deaths: 9,
-    recoveries: 32,
-  },
-  '05/04/2020': {
-    cases: 251,
-    deaths: 7,
-    recoveries: 45,
-  },
-  '04/04/2020': {
-    cases: 430,
-    deaths: 25,
-    recoveries: 35,
-  },
-  '03/04/2020': {
-    cases: 445,
-    deaths: 22,
-    recoveries: 27,
-  },
-  '02/04/2020': {
-    cases: 278,
-    deaths: 9,
-    recoveries: 15,
-  },
-  '01/04/2020': {
-    cases: 215,
-    deaths: 16,
-    recoveries: 32,
-  },
-  '31/03/2020': {
-    cases: 293,
-    deaths: 25,
-    recoveries: 40,
-  },
-  '30/03/2020': {
-    cases: 192,
-    deaths: 2,
-    recoveries: 11,
-  },
-  '29/03/2020': {
-    cases: 308,
-    deaths: 5,
-    recoveries: 30,
-  },
-  '28/03/2020': {
-    cases: 160,
-    deaths: 11,
-    recoveries: 24,
-  },
-  '27/03/2020': {
-    cases: 263,
-    deaths: 4,
-    recoveries: 21,
-  },
-  '26/03/2020': {
-    cases: 123,
-    deaths: 9,
-    recoveries: 8,
-  },
-  '25/03/2020': {
-    cases: 144,
-    deaths: 2,
-    recoveries: 7,
-  },
-  '24/03/2020': {
-    cases: 186,
-    deaths: 4,
-    recoveries: 8,
-  },
-  '23/03/2020': {
-    cases: 143,
-    deaths: 4,
-    recoveries: 7,
-  },
-  '22/03/2020': {
-    cases: 66,
-    deaths: 3,
-    recoveries: 12,
-  },
-  '21/03/2020': {
-    cases: 59,
-    deaths: 0,
-    recoveries: 21,
-  },
-  '20/03/2020': {
-    cases: 31,
-    deaths: 0,
-    recoveries: 6,
-  },
-  '19/03/2020': {
-    cases: 17,
-    deaths: 0,
-    recoveries: 6,
-  },
-  '18/03/2020': {
-    cases: 43,
-    deaths: 0,
-    recoveries: 3,
-  },
-  '17/03/2020': {
-    cases: 49,
-    deaths: 0,
-    recoveries: 10,
-  },
-  '16/03/2020': {
-    cases: 29,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '15/03/2020': {
-    cases: 16,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '14/03/2020': {
-    cases: 28,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '13/03/2020': {
-    cases: 36,
-    deaths: 0,
-    recoveries: 1,
-  },
-  '12/03/2020': {
-    cases: 12,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '11/03/2020': {
-    cases: 18,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '10/03/2020': {
-    cases: 12,
-    deaths: 0,
-    recoveries: 5,
-  },
-  '09/03/2020': {
-    cases: 2,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '08/03/2020': {
-    cases: 2,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '07/03/2020': {
-    cases: 4,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '06/03/2020': {
-    cases: 3,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '05/03/2020': {
-    cases: 0,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '04/03/2020': {
-    cases: 2,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '03/03/2020': {
-    cases: 1,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '02/03/2020': {
-    cases: 0,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '01/03/2020': {
-    cases: 0,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '28/02/2020': {
-    cases: 2,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '27/02/2020': {
-    cases: 0,
-    deaths: 0,
-    recoveries: 0,
-  },
-  '26/02/2020': {
-    cases: 1,
-    deaths: 0,
-    recoveries: 0,
-  },
-};
+  "09/06/2020": {
+    "cases": 145,
+    "recoveries": 84,
+    "deaths": 11
+  },
+  "08/06/2020": {
+    "cases": 125,
+    "recoveries": 188,
+    "deaths": 8
+  },
+  "07/06/2020": {
+    "cases": 189,
+    "deaths": 8,
+    "recoveries": 219
+  },
+  "06/06/2020": {
+    "cases": 187,
+    "deaths": 10,
+    "recoveries": 274
+  },
+  "05/06/2020": {
+    "cases": 196,
+    "deaths": 9,
+    "recoveries": 226
+  },
+  "04/06/2020": {
+    "cases": 238,
+    "deaths": 11,
+    "recoveries": 119
+  },
+  "03/06/2020": {
+    "cases": 152,
+    "deaths": 9,
+    "recoveries": 274
+  },
+  "02/06/2020": {
+    "cases": 119,
+    "deaths": 9,
+    "recoveries": 100
+  },
+  "01/06/2020": {
+    "cases": 141,
+    "deaths": 8,
+    "recoveries": 170
+  },
+  "31/05/2020": {
+    "cases": 124,
+    "deaths": 9,
+    "recoveries": 210
+  },
+  "30/05/2020": {
+    "cases": 151,
+    "deaths": 13,
+    "recoveries": 217
+  },
+  "29/05/2020": {
+    "cases": 191,
+    "deaths": 11,
+    "recoveries": 200
+  },
+  "28/05/2020": {
+    "cases": 197,
+    "deaths": 10,
+    "recoveries": 467
+  },
+  "27/05/2020": {
+    "cases": 165,
+    "deaths": 9,
+    "recoveries": 288
+  },
+  "26/05/2020": {
+    "cases": 146,
+    "deaths": 17,
+    "recoveries": 244
+  },
+  "25/05/2020": {
+    "cases": 213,
+    "deaths": 14,
+    "recoveries": 231
+  },
+  "24/05/2020": {
+    "cases": 213,
+    "deaths": 9,
+    "recoveries": 212
+  },
+  "23/05/2020": {
+    "cases": 145,
+    "deaths": 11,
+    "recoveries": 410
+  },
+  "22/05/2020": {
+    "cases": 127,
+    "deaths": 8,
+    "recoveries": 196
+  },
+  "21/05/2020": {
+    "cases": 198,
+    "deaths": 10,
+    "recoveries": 225
+  },
+  "20/05/2020": {
+    "cases": 196,
+    "deaths": 15,
+    "recoveries": 190
+  },
+  "19/05/2020": {
+    "cases": 155,
+    "deaths": 19,
+    "recoveries": 236
+  },
+  "18/05/2020": {
+    "cases": 165,
+    "deaths": 10,
+    "recoveries": 40
+  },
+  "17/05/2020": {
+    "cases": 167,
+    "deaths": 16,
+    "recoveries": 316
+  },
+  "16/05/2020": {
+    "cases": 267,
+    "deaths": 25,
+    "recoveries": 204
+  },
+  "15/05/2020": {
+    "cases": 190,
+    "deaths": 10,
+    "recoveries": 317
+  },
+  "14/05/2020": {
+    "cases": 245,
+    "deaths": 30,
+    "recoveries": 1092
+  },
+  "13/05/2020": {
+    "cases": 214,
+    "deaths": 25,
+    "recoveries": 276
+  },
+  "12/05/2020": {
+    "cases": 200,
+    "deaths": 19,
+    "recoveries": 440
+  },
+  "11/05/2020": {
+    "cases": 226,
+    "deaths": 20,
+    "recoveries": 194
+  },
+  "10/05/2020": {
+    "cases": 231,
+    "deaths": 26,
+    "recoveries": 139
+  },
+  "09/05/2020": {
+    "cases": 320,
+    "deaths": 28,
+    "recoveries": 489
+  },
+  "08/05/2020": {
+    "cases": 312,
+    "deaths": 22,
+    "recoveries": 279
+  },
+  "07/05/2020": {
+    "cases": 392,
+    "deaths": 18,
+    "recoveries": 356
+  },
+  "06/05/2020": {
+    "cases": 270,
+    "deaths": 31,
+    "recoveries": 334
+  },
+  "05/05/2020": {
+    "cases": 325,
+    "deaths": 24,
+    "recoveries": 185
+  },
+  "04/05/2020": {
+    "cases": 349,
+    "deaths": 23,
+    "recoveries": 400
+  },
+  "03/05/2020": {
+    "cases": 431,
+    "deaths": 25,
+    "recoveries": 322
+  },
+  "02/05/2020": {
+    "cases": 165,
+    "deaths": 29,
+    "recoveries": 219
+  },
+  "01/05/2020": {
+    "cases": 327,
+    "deaths": 31,
+    "recoveries": 311
+  },
+  "30/04/2020": {
+    "cases": 262,
+    "deaths": 20,
+    "recoveries": 448
+  },
+  "29/04/2020": {
+    "cases": 362,
+    "deaths": 25,
+    "recoveries": 165
+  },
+  "28/04/2020": {
+    "cases": 277,
+    "deaths": 19,
+    "recoveries": 263
+  },
+  "27/04/2020": {
+    "cases": 303,
+    "deaths": 23,
+    "recoveries": 87
+  },
+  "26/04/2020": {
+    "cases": 401,
+    "deaths": 33,
+    "recoveries": 164
+  },
+  "25/04/2020": {
+    "cases": 218,
+    "deaths": 23,
+    "recoveries": 73
+  },
+  "24/04/2020": {
+    "cases": 321,
+    "deaths": 25,
+    "recoveries": 339
+  },
+  "23/04/2020": {
+    "cases": 386,
+    "deaths": 19,
+    "recoveries": 72
+  },
+  "22/04/2020": {
+    "cases": 468,
+    "deaths": 26,
+    "recoveries": 253
+  },
+  "21/04/2020": {
+    "cases": 306,
+    "deaths": 31,
+    "recoveries": 136
+  },
+  "20/04/2020": {
+    "cases": 190,
+    "deaths": 17,
+    "recoveries": 125
+  },
+  "19/04/2020": {
+    "cases": 328,
+    "deaths": 17,
+    "recoveries": 162
+  },
+  "18/04/2020": {
+    "cases": 351,
+    "deaths": 17,
+    "recoveries": 222
+  },
+  "17/04/2020": {
+    "cases": 360,
+    "deaths": 13,
+    "recoveries": 151
+  },
+  "16/04/2020": {
+    "cases": 491,
+    "deaths": 25,
+    "recoveries": 140
+  },
+  "15/04/2020": {
+    "cases": 337,
+    "deaths": 30,
+    "recoveries": 166
+  },
+  "14/04/2020": {
+    "cases": 246,
+    "deaths": 14,
+    "recoveries": 137
+  },
+  "13/04/2020": {
+    "cases": 333,
+    "deaths": 12,
+    "recoveries": 62
+  },
+  "12/04/2020": {
+    "cases": 310,
+    "deaths": 24,
+    "recoveries": 94
+  },
+  "11/04/2020": {
+    "cases": 523,
+    "deaths": 25,
+    "recoveries": 29
+  },
+  "10/04/2020": {
+    "cases": 265,
+    "deaths": 28,
+    "recoveries": 82
+  },
+  "09/04/2020": {
+    "cases": 441,
+    "deaths": 20,
+    "recoveries": 119
+  },
+  "08/04/2020": {
+    "cases": 344,
+    "deaths": 27,
+    "recoveries": 68
+  },
+  "07/04/2020": {
+    "cases": 360,
+    "deaths": 25,
+    "recoveries": 54
+  },
+  "06/04/2020": {
+    "cases": 193,
+    "deaths": 9,
+    "recoveries": 32
+  },
+  "05/04/2020": {
+    "cases": 251,
+    "deaths": 7,
+    "recoveries": 45
+  },
+  "04/04/2020": {
+    "cases": 430,
+    "deaths": 25,
+    "recoveries": 35
+  },
+  "03/04/2020": {
+    "cases": 445,
+    "deaths": 22,
+    "recoveries": 27
+  },
+  "02/04/2020": {
+    "cases": 278,
+    "deaths": 9,
+    "recoveries": 15
+  },
+  "01/04/2020": {
+    "cases": 215,
+    "deaths": 16,
+    "recoveries": 32
+  },
+  "31/03/2020": {
+    "cases": 293,
+    "deaths": 25,
+    "recoveries": 40
+  },
+  "30/03/2020": {
+    "cases": 192,
+    "deaths": 2,
+    "recoveries": 11
+  },
+  "29/03/2020": {
+    "cases": 308,
+    "deaths": 5,
+    "recoveries": 30
+  },
+  "28/03/2020": {
+    "cases": 160,
+    "deaths": 11,
+    "recoveries": 24
+  },
+  "27/03/2020": {
+    "cases": 263,
+    "deaths": 4,
+    "recoveries": 21
+  },
+  "26/03/2020": {
+    "cases": 123,
+    "deaths": 9,
+    "recoveries": 8
+  },
+  "25/03/2020": {
+    "cases": 144,
+    "deaths": 2,
+    "recoveries": 7
+  },
+  "24/03/2020": {
+    "cases": 186,
+    "deaths": 4,
+    "recoveries": 8
+  },
+  "23/03/2020": {
+    "cases": 143,
+    "deaths": 4,
+    "recoveries": 7
+  },
+  "22/03/2020": {
+    "cases": 66,
+    "deaths": 3,
+    "recoveries": 12
+  },
+  "21/03/2020": {
+    "cases": 59,
+    "deaths": 0,
+    "recoveries": 21
+  },
+  "20/03/2020": {
+    "cases": 31,
+    "deaths": 0,
+    "recoveries": 6
+  },
+  "19/03/2020": {
+    "cases": 17,
+    "deaths": 0,
+    "recoveries": 6
+  },
+  "18/03/2020": {
+    "cases": 43,
+    "deaths": 0,
+    "recoveries": 3
+  },
+  "17/03/2020": {
+    "cases": 49,
+    "deaths": 0,
+    "recoveries": 10
+  },
+  "16/03/2020": {
+    "cases": 29,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "15/03/2020": {
+    "cases": 16,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "14/03/2020": {
+    "cases": 28,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "13/03/2020": {
+    "cases": 36,
+    "deaths": 0,
+    "recoveries": 1
+  },
+  "12/03/2020": {
+    "cases": 12,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "11/03/2020": {
+    "cases": 18,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "10/03/2020": {
+    "cases": 12,
+    "deaths": 0,
+    "recoveries": 5
+  },
+  "09/03/2020": {
+    "cases": 2,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "08/03/2020": {
+    "cases": 2,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "07/03/2020": {
+    "cases": 4,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "06/03/2020": {
+    "cases": 3,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "05/03/2020": {
+    "cases": 0,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "04/03/2020": {
+    "cases": 2,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "03/03/2020": {
+    "cases": 1,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "02/03/2020": {
+    "cases": 0,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "01/03/2020": {
+    "cases": 0,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "28/02/2020": {
+    "cases": 2,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "27/02/2020": {
+    "cases": 0,
+    "deaths": 0,
+    "recoveries": 0
+  },
+  "26/02/2020": {
+    "cases": 1,
+    "deaths": 0,
+    "recoveries": 0
+  }
+}
