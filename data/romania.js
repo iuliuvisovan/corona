d1c0da00273542b8033da4a8356f94b654a1adec
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-07-16 13:05:45 +0300

    ro 1pm

--- data/romania.js
+++ data/romania.js
@@ -1,4 +1,10 @@
 window.romaniaData = {
+  "16/07/2020": {
+    "cases": 0,
+    "recoveries": 0,
+    "deaths": 0,
+    "tests": 0
+  },
   "15/07/2020": {
     "cases": 641,
     "recoveries": 357,
